f30cc85fc9500e9386435b5eef818a371ecf9ac0
theodore-chronopoulos <el17021@mail.ntua.gr>
2022-02-10 10:23:52 +0200

    sql query 2 is done, sql query 3 almost

---
+++ sql_query2.py
@@ -0,0 +1,53 @@
+from select import select
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("query1-rdd").getOrCreate()
+spark.conf.set("spark.sql.crossJoin.enabled", "true")
+
+def average_func(average):
+    average = round(average, 2)
+    return average
+
+def percentage_func(plusthree, total):
+    average = (plusthree / total) * 100
+    average = round(average, 2)
+    return average
+
+ratings = spark.read.format('csv'). \
+			options(header='false',
+				inferSchema='true'). \
+			load("hdfs://master:9000/files/ratings.csv")
+
+ratings.registerTempTable("ratings")
+spark.udf.register("average_func", average_func)
+spark.udf.register("percentage_func", percentage_func)
+
+sqlString1 = \
+    "select _c0 as User_ID, average_func(avg(_c2)) as Avg_Stars "  + \
+	"from ratings " + \
+    "group by User_ID "
+
+sqlString2 = \
+    "select count(User_ID) as Num_of_Users_3plus "  + \
+	"from average_ratings " + \
+    "where  Avg_Stars > 3 "
+
+sqlString3 = \
+    "select count(User_ID) as Num_of_Users "  + \
+	"from average_ratings " 
+
+sqlString4 = \
+    "select percentage_func(num_of_users_3plus.Num_of_Users_3plus, num_of_users.Num_of_Users) as Percentage "  + \
+	"from num_of_users_3plus, num_of_users " 
+
+res = spark.sql(sqlString1)
+res.registerTempTable("average_ratings")
+res.show()
+res2 = spark.sql(sqlString2)
+res2.registerTempTable("num_of_users_3plus")
+res2.show()
+res3 = spark.sql(sqlString3)
+res3.registerTempTable("num_of_users")
+res3.show()
+res4 = spark.sql(sqlString4)
+res4.show()
---
+++ sql_query3.py
@@ -0,0 +1,58 @@
+from select import select
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("query1-rdd").getOrCreate()
+spark.conf.set("spark.sql.crossJoin.enabled", "true")
+
+# def average_func(average):
+#     average = round(average, 2)
+#     return average
+
+movie_genres = spark.read.format('csv'). \
+			options(header='false',
+				inferSchema='true'). \
+			load("hdfs://master:9000/files/movie_genres.csv")
+
+movie_genres.registerTempTable("movie_genres")
+
+ratings = spark.read.format('csv'). \
+			options(header='false',
+				inferSchema='true'). \
+			load("hdfs://master:9000/files/ratings.csv")
+
+ratings.registerTempTable("ratings")
+# spark.udf.register("average_func", average_func)
+
+sqlString1 = \
+    "select count(_c0) as Movies_Per_Category, _c1 as Genres "  + \
+	"from movie_genres " + \
+    "group by Genres "
+
+sqlString2 = \
+    "select _c1 as Movie_ID, avg(_c2) as Avg_Stars "  + \
+	"from ratings " + \
+    "group by Movie_ID "
+
+
+sqlString3 = \
+    "select avg(avg_per_movie.Avg_Stars) as Avg_per_Cat, movie_genres._c1 as Genre "  + \
+	"from avg_per_movie, movie_genres " + \
+    "where avg_per_movie.Movie_ID == movie_genres._c0 " + \
+    "group by movie_genres._c1" 
+
+sqlString4 = \
+    "select avg_per_cat.Genre as Genre, avg_per_cat.Avg_per_Cat as Avg_per_Cat, movies_per_cat.Movies_Per_Category as Movies_Per_Category "  + \
+	"from avg_per_cat, movies_per_cat " + \
+    "where avg_per_cat.Genre == movies_per_cat.Genres " 
+
+res = spark.sql(sqlString1)
+res.registerTempTable("movies_per_cat")
+res.show()
+res2 = spark.sql(sqlString2)
+res2.registerTempTable("avg_per_movie")
+res2.show()
+res3 = spark.sql(sqlString3)
+res3.registerTempTable("avg_per_cat")
+res3.show()
+res4 = spark.sql(sqlString4)
+res4.show()
