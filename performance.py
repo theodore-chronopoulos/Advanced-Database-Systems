d1c68c57cef04ba93ad01c4d6733cc8204e255cf
theodore-chronopoulos <el17021@mail.ntua.gr>
2022-02-16 12:28:31 +0200

    only q3 q4 q5 in rdd are to be done

--- performance.py
+++ performance.py
@@ -1,6 +1,6 @@
 
 
 # [query1, query2, query3, query4, query5] in seconds
-rdd_array = []
-spark_csv_array = [24.171, 113.89, 115.496, 23.65, 218.05]
-spark_parquet_array = [20.17, 26.343, 30.182, 19.805, 129.55]
+rdd_array = [8.449, 141.898,]
+spark_csv_array = [24.171, 108.698, 115.496, 23.65, 218.05]
+spark_parquet_array = [20.17, 22.603, 30.182, 19.805, 129.55]
--- query2RDDAPI.py
+++ query2RDDAPI.py
@@ -19,7 +19,7 @@ rdd = \
 	sc.textFile("hdfs://master:9000/files/ratings.csv"). \
 	map(lambda x : split_complex(x))
 
-rdd1 = rdd.map(lambda x : (int(x[1]),int(x[0]),float(x[2])))
+rdd1 = rdd.map(lambda x : (int(x[0]),int(x[1]),float(x[2])))
 rdd2 = rdd1.map(lambda x : (x[0],(1,x[2])))
 
 rdd3 = rdd2.reduceByKey(lambda x,y: (x[0]+y[0],x[1] + y[1]))
@@ -28,7 +28,7 @@ rdd5 = rdd4.filter(lambda x: x[1][1]>3)
 rdd6 = rdd5.reduceByKey(lambda x,y: (x[0]+y[0],"a"))
 rdd7 = rdd4.reduceByKey(lambda x,y: (x[0]+y[0],"b"))
 rdd8 = rdd6.join(rdd7)
-rdd9 = rdd8.map(lambda x: x[1][0][0]/x[1][1][0])
+rdd9 = rdd8.map(lambda x: 100 * x[1][0][0] / x[1][1][0])
 
 # def toCSVLine(data):
 #   return ','.join(str(d) for d in data)
